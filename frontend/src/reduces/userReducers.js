a38e3fca90c92d6e895a68de2d97951c19f44f47
Maksood293 <maksoodkhan0143@gmail.com>
2021-06-08 20:20:33 +0530

    User register screen & redux state & register router in backend have created

--- frontend/src/reduces/userReducers.js
+++ frontend/src/reduces/userReducers.js
@@ -1,4 +1,7 @@
 import {
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
   USER_SINGIN_FAIL,
   USER_SINGIN_REQUEST,
   USER_SINGIN_SUCCESS,
@@ -16,6 +19,18 @@ export const userSinginReducer = (state = {}, action) => {
     case USER_SINGOUT:
       return {};
 
+    default:
+      return state;
+  }
+};
+export const userRegisterReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true };
+    case USER_REGISTER_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+    case USER_REGISTER_FAIL:
+      return { loading: false, error: action.payload };
     default:
       return state;
   }

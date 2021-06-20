d362ed267391cc0ff344d62d69526544e8a4cf67
Maksood293 <maksoodkhan0143@gmail.com>
2021-06-20 07:44:24 +0530

    backend api and database schema created for place-order-screen

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
+import orderRouter from "./routers/orderRouter.js";
 
 dotenv.config(); //to use env file we need to config in backend
 
@@ -32,6 +33,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter); //order router api
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
